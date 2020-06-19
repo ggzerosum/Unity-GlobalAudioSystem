f97a3b67d603fbf79493880abacc94ba2d3ddce4
Han-Seunghun <ggzerosum@naver.com>
2020-06-20 01:36:04 +0900

    Audio Clip Test Code Added

---
+++ Samples/UnitTest_AudioClipPlayer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ProvisGames.Core.AudioSystem.UnitTest
+{
+    internal class UnitTest_AudioClipPlayer : MonoBehaviour
+    {
+        public AudioClip clip;
+        public int track;
+        public bool isLoop;
+
+
+        public void PlayAdditive()
+        {
+            ServiceLocator.Instance
+                ?.GetGlobalAudioService()
+                ?.Play(track, new GlobalAudioService.AudioSetting(clip, isLoop), GlobalAudioService.PlayMode.Additive);
+        }
+        public void PlaySingle()
+        {
+            ServiceLocator.Instance
+                ?.GetGlobalAudioService()
+                ?.Play(track, new GlobalAudioService.AudioSetting(clip, isLoop), GlobalAudioService.PlayMode.Single);
+        }
+    }
+}
