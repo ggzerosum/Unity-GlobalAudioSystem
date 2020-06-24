4cf09b8706a392ee72635832aa315cc5766580ca
Han-Seunghun <ggzerosum@naver.com>
2020-06-24 19:05:37 +0900

    오디오 트랙 알고리즘 대거 수정
    PlayAudio, Mixing의 알고리즘이 서로 붙어있던 것을 분리하여 3개의 함수로 분화
    (PlayAudio, DoMixing / StopMixing)
    
    오디오의 실행과 Mix 기능의 실행을 분리하여 조금 더 명확한 API를 제공하기위해 노력하였다.

--- Runtime/AudioService/AudioTrack.cs
+++ Runtime/AudioService/AudioTrack.cs
@@ -44,9 +44,11 @@ namespace ProvisGames.Core.AudioSystem
         }
 
         // Mixing
-        private int mixingPivot = -1;
-        private bool allowMix = false;
-        private bool isMixing = false;
+        private int _mixingPivot = -1;
+        private bool _allowMix = false;
+        private bool _isMixing = false;
+        // 현재 Track이 Mixing 기능을 실행중인 지 확인합니다.
+        public bool IsMixing => this._isMixing;
 
         // Dirty Flag
         private bool shouldApplyOptionWhenPossible = false;
@@ -80,6 +82,102 @@ namespace ProvisGames.Core.AudioSystem
             }
         }
 
+
+        // AudioPlayer를 실행 목록에 올립니다. 추가된 AudioPlayer는 자동으로 실행됩니다.
+        public void PlayAudio(GlobalAudioService.AudioSetting setting, GlobalAudioService.PlayMode mode/*, bool mix*/)
+        {
+            // Mix와 Audio Play의 알고리즘은 철저히 분리되어야하며, Pivot을 통해서만 서로 제약해야한다.
+            AudioSource source = this.sharedAudioSourcePool.Get();
+            source.clip = setting.Clip;
+            source.loop = setting.IsLoop;
+            source.volume = this.trackVolume;
+            AudioPlayer audioPlayer = new AudioPlayer(source, new AudioPlayer.AudioVolume(0.0f, this.trackVolume), State.Play);
+
+            if (_isMixing)
+            {
+                // 믹싱 중일 때 Single을 사용하면 Pivot을 갱신시켜야한다. Additive는 Pivot을 건드리면 안된다.
+                if (mode == GlobalAudioService.PlayMode.Single)
+                {
+                    // Pivot을 추가할 오디오 플레이어의 위치로 미리 옮깁니다.
+                    MoveMixingPivot(audioSources.Count - _mixingPivot);
+                }
+
+                // Mixing 중일 때 새로 추가된 Audio는 Mixer가 필요로하는 AudioPlayer의 초기 세팅을 적용해준다.
+                trackMixer.AttuneAudioPlayerToMixer(ref audioPlayer);
+            }
+            else // Mixing 중이 아닐 때
+            {
+                // Pivot을 추가할 오디오 플레이어의 위치로 미리 옮깁니다.
+                MoveMixingPivot(audioSources.Count - _mixingPivot);
+
+                if (mode == GlobalAudioService.PlayMode.Single) // And Just Sigle Clip should be play
+                {
+                    Stop();
+                }
+                else if (mode == GlobalAudioService.PlayMode.Additive)// Mode == Additive
+                {
+                    // 반드시 아무것도 하지 말아야한다.
+                }
+            }
+
+            // 오디오플레이어를 추가함으로써 미리 옮겨둔 Pivot과 위치가 동일해진다.
+            audioSources.Add(audioPlayer);
+            source.Play();
+        }
+
+        // Track의 모든 오디오를 강제 종료시킵니다.
+        public void Stop()
+        {
+            // Always Stop Immediately now
+            StopMixing(true);
+
+            for (int i = 0; i < audioSources.Count; i++)
+            {
+                audioSources[i].StopAndEjectClip();
+                RemoveFlag(i, State.Play);
+            }
+        }
+
+        // Track에서 사용할 Mixer를 교체합니다.
+        public void SetTrackMixer(Mixer<AudioPlayer> mixer)
+        {
+            // 믹싱이 진행중 일 때, 같은 타입의 Mixer가 투입될 경우 무시해야함.
+            if (_isMixing && this.trackMixer.GetType() == mixer.GetType())
+            {
+                Debug.Log("Same type of Mixer Ignored while mixing");
+                return;
+            }
+
+            this.trackMixer = mixer;
+        }
+
+        // Mix 기능을 실행할 수 있으면 현재 트랙에서 Mix를 실행합니다.
+        public void DoMixing()
+        {
+            if (CanMix())
+            {
+                DoMixBehaviour();
+            }
+        }
+
+        // Mix를 강제로 종료합니다.
+        // immediate true: mixer를 즉시 종료 시킵니다.
+        // immediate false: 다음 업데이트때 mixer 종료가 반영됩니다.
+        public void StopMixing(bool immediate = true)
+        {
+            EndMixing();
+
+            if (immediate)
+                UpdateMixing(0);
+        }
+
+        public void SetVolume(float volume)
+        {
+            this.trackVolume = Mathf.Clamp01(volume);
+            this.isOptionDataDirty = true;
+        }
+
+
         private void InspectAudioList()
         {
             var removableList = ListPool<int>.Get();
@@ -105,7 +203,7 @@ namespace ProvisGames.Core.AudioSystem
             for (int i = removableList.Count - 1; i >= 0; i--) // end to front, for avoid index change
             {
                 int indexOfAudioSource = removableList[i];
-                ReMapPivotWhenElementRemoved(audioSources.Count, indexOfAudioSource);
+                ReMapMixingPivotWhenElementRemoved(audioSources.Count, indexOfAudioSource);
 
                 AudioSource audiosource = this.audioSources[indexOfAudioSource].Audio;
                 audioSources.RemoveAt(indexOfAudioSource); // Remove Inverse. This algorithm doesn't change index of list when remove
@@ -123,11 +221,12 @@ namespace ProvisGames.Core.AudioSystem
         /// <returns>when Mixing, return true else return false</returns>
         private bool UpdateMixing(float deltaTime)
         {
-            if (allowMix && !isMixing)
+            if (_allowMix && !_isMixing)
             {
-                if (mixingPivot <= 0)
+                if (!CanMix())
                 {
                     EndMixing();
+                    return false;
                 }
 
                 // Notify Begin Mixing
@@ -135,14 +234,14 @@ namespace ProvisGames.Core.AudioSystem
             }
 
             // Mix Audio
-            if (isMixing)
+            if (_isMixing)
             {
                 // Get List from Pool
                 var left = ListPool<AudioPlayer>.Get();
                 var right = ListPool<AudioPlayer>.Get();
 
                 // there's one or no audio available
-                if (mixingPivot <= 0)
+                if (!CanMix())
                 {
                     EndMixing();
                     ListPool<AudioPlayer>.Release(left);
@@ -204,13 +303,13 @@ namespace ProvisGames.Core.AudioSystem
         private void BeginMixing()
         {
             trackMixer.BeginMix();
-            isMixing = true;
+            _isMixing = true;
         }
         
         // EndMixing과 Play Audio입력이 경쟁상태에 빠져서는 안된다. 동기함수로 작성할 것을 강제한다.
         private void EndMixing()
         {
-            if (isMixing)
+            if (_isMixing)
             {
                 trackMixer.EndMix();
 
@@ -223,11 +322,11 @@ namespace ProvisGames.Core.AudioSystem
                 }
             }
 
-            allowMix = false;
-            isMixing = false;
+            _allowMix = false;
+            _isMixing = false;
         }
 
-        private void ReMapPivotWhenElementRemoved(int arrayLength, int removeIndex)
+        private void ReMapMixingPivotWhenElementRemoved(int arrayLength, int removeIndex)
         {
             // 배열의 길이가 0일 경우에는 Pivot을 움직일 공간 자체가 없으므로 예외처리한다.
             if (arrayLength == 0)
@@ -235,41 +334,41 @@ namespace ProvisGames.Core.AudioSystem
 
             if (arrayLength == 1) // removable Index is just one (0 index)
             {
-                ResetPivot();
+                ResetMixingPivot();
             }
 
             // Pivot 좌측의 Index를 삭제할 경우, Pivot을 좌측으로 당겨야한다.
-            if (removeIndex < this.mixingPivot)
+            if (removeIndex < this._mixingPivot)
             {
-                DecrementPivot();
+                DecrementMixingPivot();
             }
             // Pivot의 위치와 동일한 Index를 삭제할 경우, Index가 배열의 마지막 요소였을 경우 Pivot을 배열의 길이에맞게 한칸 좌측으로 당겨야한다.
-            else if (removeIndex == this.mixingPivot)
+            else if (removeIndex == this._mixingPivot)
             {
                 if (arrayLength - 1 == removeIndex) // if remove Index is last index of array
                 {
-                    DecrementPivot();
+                    DecrementMixingPivot();
                 }
             }
 
             // Pivot을 건드리지않고 그대로 둔다.
         }
-        private void IncrementPivot()
+        private void IncrementMixingPivot()
         {
-            MovePivot(1);
+            MoveMixingPivot(1);
         }
-        private void DecrementPivot()
+        private void DecrementMixingPivot()
         {
-            MovePivot(-1);
+            MoveMixingPivot(-1);
         }
-        private void ResetPivot()
+        private void ResetMixingPivot()
         {
             // 언제나 Pivot을 -1로 설정하기위함.
-            MovePivot(-(mixingPivot + 1));
+            MoveMixingPivot(-(_mixingPivot + 1));
         }
-        private void MovePivot(int delta)
+        private void MoveMixingPivot(int delta)
         {
-            mixingPivot = Mathf.Clamp(mixingPivot + delta, -1, int.MaxValue);
+            _mixingPivot = Mathf.Clamp(_mixingPivot + delta, -1, int.MaxValue);
         }
 
 
@@ -279,7 +378,7 @@ namespace ProvisGames.Core.AudioSystem
             // Left : mix from, Right : mix to
             for (int i = 0; i < audioSources.Count; i++)
             {
-                if (i < mixingPivot)
+                if (i < _mixingPivot)
                 {
                     left.Add(audioSources[i]);
                     onLeft(i);
@@ -292,73 +391,16 @@ namespace ProvisGames.Core.AudioSystem
             }
         }
 
-        public void SetTrackMixer(Mixer<AudioPlayer> mixer)
-        {
-            if (this.trackMixer.GetType().IsAssignableFrom((mixer.GetType())))
-            {
-                Debug.Log("Same type of Mixer Ignored");
-                return;
-            }
 
-            this.trackMixer = mixer;
+        private bool CanMix()
+        {
+            // Pivot은 Index이므로 갯수 비교를 위해 +1 을 실시하였다.
+            // Pivot always (except 0) same as total count - 1
+            return _mixingPivot + 1 >= trackMixer.MinimumRequirementsCount;
         }
-
-        public void Play(GlobalAudioService.AudioSetting setting, GlobalAudioService.PlayMode mode, bool mix)
+        private void DoMixBehaviour()
         {
-            AudioSource source = this.sharedAudioSourcePool.Get();
-            source.clip = setting.Clip;
-            source.loop = setting.IsLoop;
-            source.volume = this.trackVolume;
-            AudioPlayer audioPlayer = new AudioPlayer(source, new AudioPlayer.AudioVolume(0.0f, this.trackVolume), State.Play);
-
-            if (mix)
-            {
-                // If Mode is Single, Pivot always move to lastest player
-                // If Mode is Additive, Pivot stay while mixing
-                if (!isMixing || mode == GlobalAudioService.PlayMode.Single)
-                {
-                    // Pivot을 마지막 오디오 플레이어의 위치로 옮깁니다.
-                    MovePivot(audioSources.Count - mixingPivot); // Update should be on main thread.
-                }
-
-                // Mixing Should applied When element count >= 2
-                if (mixingPivot > 0) // Pivot always (except 0) same as total count - 1
-                {
-                    trackMixer.SettingTarget(audioPlayer);
-                    allowMix = true;
-                }
-            }
-            else
-            {
-                // Not Mix Mode
-                if (mode == GlobalAudioService.PlayMode.Single) // And Just Sigle Clip should be play
-                {
-                    Stop(); // Stop all audio in track that has been playing
-                }
-                else if (mode == GlobalAudioService.PlayMode.Additive)// Mode == Additive
-                {
-                    // 반드시 아무것도 하지 말아야한다.
-                }
-            }
-
-            audioSources.Add(audioPlayer);
-            source.Play();
-        }
-        public void Stop()
-        {
-            // Always Stop Immediately now
-            EndMixing();
-            for (int i = 0; i < audioSources.Count; i++)
-            {
-                audioSources[i].StopAndEjectClip();
-                RemoveFlag(i, State.Play);
-            }
-        }
-
-        public void SetVolume(float volume)
-        {
-            this.trackVolume = Mathf.Clamp01(volume);
-            this.isOptionDataDirty = true;
+            _allowMix = true;
         }
 
         private bool HasFlag(int index, State check)
